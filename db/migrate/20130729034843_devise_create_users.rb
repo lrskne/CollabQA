c73ad93e74ec93b905ff10a1b02ecbd14c5b521e
Laura Erskine <lrskne@gmail.com>
2013-07-28 23:26:52 -0700

    Some migration changes to the join table

--- db/migrate/20130729034843_devise_create_users.rb
+++ db/migrate/20130729034843_devise_create_users.rb
@@ -39,6 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
+    add_index :questions_users, [question_id, user_id]
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
--- db/migrate/20130729053401_question_user_join.rb.rb
+++ db/migrate/20130729053401_question_user_join.rb.rb
@@ -1,13 +1,10 @@
 class QuestionUserJoin < ActiveRecord::Migration
-  def up
-    create_table 'questions_users', id => false do |t|
-      t.references :question
-      t.references :user
+  def change
+    create_table :questions_users, id: false do |t|
+      t.belongs_to :question
+      t.belongs_to :user
     end
   end
-  # did not add the :unique => true for now
-  add_index(:questions_users, [question_id, user_id])
-  def down
-    drop table 'questions_users'
-  end
 end
+
+
