b7bbdc3312e60407221ddb0e4a78007815c854c3
Laura Erskine <lrskne@gmail.com>
2013-07-28 22:08:51 -0700

    Some user/devise entered, no models yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollabQA::Application.config.secret_token = 'b97ef8377e10bcac7c238338d2a6275594e1db63023c060814744c68df5d3abb2ca6fb6513f77c1f2419bbf4e97940b04b037843f39bc34b2c57e3b3b840c108'
